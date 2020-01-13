b94b90d6a937ee7f7294b646f74b85e96a0091db
IgumenovMaxim <igumenovmaxim@gmail.com>
2020-01-13 22:54:34 +0200

    Add scenes

---
+++ Assets/Scripts/Game/MainMenu/UI/UI_MainMenu_PopUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_MainMenu_PopUp : MonoBehaviour
+{
+    public Button nextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextLevel.onClick.AddListener(ToNextLevel);
+    }
+
+    private void ToNextLevel() {
+        SceneController.SelectScene("Journey", gameObject.scene.name, TypeLastScene.Wait);
+    }
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EnterTextSpace;
+using System;
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Action<string> ChangeAction = (str) => { Debug.LogError(str); };
+        Action GoodAction = () => { Debug.LogError("GoodAction"); };
+        Action BedAction = () => { Debug.LogError("BedAction"); EnterTextController.Remove("test"); };
+
+        EnterTextController.Add("test", ChangeAction, GoodAction, BedAction);
     }
 
     // Update is called once per frame
@@ -15,4 +21,6 @@ public class Test : MonoBehaviour
     {
         
     }
+
+    
 }
