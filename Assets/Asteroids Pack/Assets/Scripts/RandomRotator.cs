813a00ec4736fcec37a83bf33fc75bdd3c38852f
Max <unity3damcon@gmail.com>
2020-05-25 08:12:42 +0300

    Work with prefabs

--- Assets/Asteroids Pack/Assets/Scripts/RandomRotator.cs
+++ Assets/Asteroids Pack/Assets/Scripts/RandomRotator.cs
@@ -4,10 +4,23 @@ using System.Collections;
 public class RandomRotator : MonoBehaviour
 {
     [SerializeField]
-    private float tumble;
+    private float tumble = 0.25f;
+    [Space]
+    public float _randomPositionRange = 1;
+    private float RandomPositionRange { get { return Random.Range(-_randomPositionRange, _randomPositionRange); } }
 
     void Start()
     {
         GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+    private void StartRotate() {
+        transform.localRotation = Quaternion.Euler(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
+    }
+
+    [ContextMenu("SetRandomPosition")]
+    public void SetRandomPosition() {
+        StartRotate();
+        transform.position = new Vector3(RandomPositionRange, RandomPositionRange, RandomPositionRange);
     }
 }
